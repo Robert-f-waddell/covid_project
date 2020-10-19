b1a2d4b659d664dda64883612775498325885601
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-19 17:23:35 +0100

    Rename app1.py to app11.py

---
+++ apps/app11.py
@@ -0,0 +1,29 @@
+
+  
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.graph_objects as go
+import pandas as pd
+from dash.dependencies import Input, Output
+
+from app import app
+
+pp.layout = html.Div(
+    children=[
+        html.Div(className='row',
+                 children=[
+                    html.Div(className='four columns div-user-controls',
+                             children=[
+                                 html.H2('Home page'),
+                                 html.P('Coronavirus dashboard using dash - plotly.'),
+                                 html.Div(id='app-1-display-value'),
+                                dcc.Link('Go to App 2', href='/apps/app2')
+                                ]
+                             ),
+                    html.Div(className='eight columns div-for-charts bg-grey',
+                             children=[])
+                              ])
+        ]
+
+)
