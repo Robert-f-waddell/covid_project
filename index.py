4faf7f07d9fcee132a7c2c56d4f4a0289ff87e4e
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-20 15:25:55 +0100

    Update index.py

--- index.py
+++ index.py
@@ -6,37 +6,26 @@ from app import app
 from apps import app1, app2
 from app import server
 
-app.layout = html.Div(
-    children=[
-        html.Div(className='row',
-                 children=[
-                    html.Div(
-                             className='four columns div-user-controls',
-                             children=[
-                                 dcc.Location(id='url', refresh=False),
-                                 html.H2('Home page'),
-                                 html.P('Coronavirus dashboard using dash - plotly.'),
-                                 html.Div(id='page-content'),
-                                dcc.Link('Go to App 1', href='/apps/app1'),
-                                dcc.Link('Go to App 2', href='/apps/app2') 
-                                ]
-                             ),
-                    html.Div(className='eight columns div-for-charts bg-grey')
-                              ])
-        ]
-
-)
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div([
+        dcc.Link('app1', href='/apps/app1'),
+        dcc.Link('app2', href='/apps/app2'),
+    ], className="row"),
+    html.Div(id='page-content', children=[])
+])
 
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/app1':
-        return app1.layout
-    elif pathname == '/apps/app2':
-        return app2.layout
+    if pathname == '/apps/vgames':
+        return vgames.layout
+    if pathname == '/apps/global_sales':
+        return global_sales.layout
     else:
-        return '404'
+        return "404 Page Error! Please choose a link"
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
