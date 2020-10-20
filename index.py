65d5929f731e4ce966a7e463d502990a8e9955c6
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-20 15:58:11 +0100

    Update index.py

--- index.py
+++ index.py
@@ -6,6 +6,19 @@ from app import app
 from apps import app1, app2
 from app import server
 
+covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+covid_df = covid_df[["continent",
+                     "location",
+                     "date",
+                     "total_cases",
+                    "new_cases",
+                    "new_cases_smoothed",
+                    "total_deaths",
+                    "new_deaths",
+                    "new_deaths_smoothed",]]
+
+covid_df = covid_df[covid_df["total_cases"].notna()]
+
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
