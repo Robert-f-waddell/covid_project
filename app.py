93612dce54a53add13f393463be46ceca4060552
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-20 16:04:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
 import dash
-import pandas as pd
-covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-covid_df = covid_df[["continent",
-                     "location",
-                     "date",
-                     "total_cases",
-                    "new_cases",
-                    "new_cases_smoothed",
-                    "total_deaths",
-                    "new_deaths",
-                    "new_deaths_smoothed",]]
-
-covid_df = covid_df[covid_df["total_cases"].notna()]
 
 app = dash.Dash(__name__)
 server = app.server
