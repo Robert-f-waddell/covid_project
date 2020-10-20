74d9b902bccd88c5baca362b7f84b09fadbc352f
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-20 16:05:07 +0100

    Update app2.py

--- apps/app2.py
+++ apps/app2.py
@@ -6,6 +6,19 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 from app import app
+import pandas as pd
+covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+covid_df = covid_df[["continent",
+                     "location",
+                     "date",
+                     "total_cases",
+                    "new_cases",
+                    "new_cases_smoothed",
+                    "total_deaths",
+                    "new_deaths",
+                    "new_deaths_smoothed",]]
+
+covid_df = covid_df[covid_df["total_cases"].notna()]
 
 
 country_names =covid_df.location.unique()
