d1ed287a981ba3aad3cd260342b84303220eadb3
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-21 23:34:30 +0100

    Update index.py

--- index.py
+++ index.py
@@ -10,9 +10,9 @@ from app import server
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
-        dcc.Link('Global Statistics| ', href='/apps/app1'),
-        dcc.Link('National Statistics| ', href='/apps/app2'),
-        dcc.Link('National Statistics v2', href='/apps/app3')
+        dcc.Link('Global Statistics|', href='/apps/app1'),
+        dcc.Link('National Statistics|', href='/apps/app2'),
+        dcc.Link('National Statistics v2', href='/apps/app3'),
     ], className="row"),
     html.Div(id='page-content', children=[])
 ])
@@ -23,9 +23,9 @@ app.layout = html.Div([
 def display_page(pathname):
     if pathname == '/apps/app1':
         return app1.layout
-    elif pathname == '/apps/app2':
+    if pathname == '/apps/app2':
         return app2.layout
-    elif pathname == '/apps/app3':
+    if pathname == '/apps/app3':
          return app3.layout
     else:
         return ""
