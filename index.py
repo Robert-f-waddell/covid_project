02548a154193231c9bc2576d9ad6d58893dce0f7
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-21 23:31:38 +0100

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from apps import app1, app2
+from apps import app1, app2, app3
 from app import server
 
 
@@ -12,7 +12,7 @@ app.layout = html.Div([
     html.Div([
         dcc.Link('Global Statistics| ', href='/apps/app1'),
         dcc.Link('National Statistics| ', href='/apps/app2'),
-#         dcc.Link('National Statistics v2', href='/apps/app3'),
+        dcc.Link('National Statistics v2', href='/apps/app3')
     ], className="row"),
     html.Div(id='page-content', children=[])
 ])
@@ -23,10 +23,10 @@ app.layout = html.Div([
 def display_page(pathname):
     if pathname == '/apps/app1':
         return app1.layout
-    if pathname == '/apps/app2':
+    elif pathname == '/apps/app2':
         return app2.layout
-#     if pathname == '/apps/app3':
-#         return app3.layout
+    elif pathname == '/apps/app3':
+         return app3.layout
     else:
         return ""
 
