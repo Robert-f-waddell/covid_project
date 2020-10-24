f9644147b93b0319269a2f811f3a1fee2b08d53b
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-24 21:17:32 +0100

    Update app1.py

--- apps/app1.py
+++ apps/app1.py
@@ -1,27 +1,26 @@
-# import plotly.express as px
-# from datetime import datetime as dt
-# import pandas as pd
+import plotly.express as px
+from datetime import datetime as dt
+import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-# from plotly.subplots import make_subplots
-# import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 from app import app
-from index import covid_df
-# covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-# covid_df = covid_df[["iso_code",
-#                     "continent",
-#                      "location",
-#                      "date",
-#                      "total_cases",
-#                     "new_cases",
-#                     "new_cases_smoothed",
-#                     "total_deaths",
-#                     "new_deaths",
-#                     "new_deaths_smoothed",]]
-# covid_df = covid_df[covid_df["total_cases"].notna()]
+covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+covid_df = covid_df[["iso_code",
+                    "continent",
+                     "location",
+                     "date",
+                     "total_cases",
+                    "new_cases",
+                    "new_cases_smoothed",
+                    "total_deaths",
+                    "new_deaths",
+                    "new_deaths_smoothed",]]
+covid_df = covid_df[covid_df["total_cases"].notna()]
 covid_df['m_date'] = pd.to_datetime(covid_df["date"], format='%Y-%m-%d').apply(lambda x: x.strftime('%Y-%m'))
 covid_df1 = covid_df[covid_df["iso_code"] != "OWID_WRL"]
 months = covid_df.m_date.unique()
