c234904c04b550f62e424cee54bfd14c8ebdcfc0
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-24 20:35:18 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,28 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+import plotly.express as px
+from datetime import datetime as dt
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 from app import app
 from apps import app1, app2,app3
 from app import server
+covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
+covid_df = covid_df[["iso_code",
+                    "continent",
+                     "location",
+                     "date",
+                     "total_cases",
+                    "new_cases",
+                    "new_cases_smoothed",
+                    "total_deaths",
+                    "new_deaths",
+                    "new_deaths_smoothed",]]
+covid_df = covid_df[covid_df["total_cases"].notna()]
+
+
 
 
 app.layout = html.Div([
