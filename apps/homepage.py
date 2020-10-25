6ee0c213c5381c8e6dd652f23768ac5ec19363dd
Robert-f-waddell <72701476+Robert-f-waddell@users.noreply.github.com>
2020-10-25 11:22:53 +0000

    Update homepage.py

--- apps/homepage.py
+++ apps/homepage.py
@@ -1,5 +1,3 @@
-import plotly.express as px
-from datetime import datetime as dt
 import pandas as pd
 import dash
 import dash_core_components as dcc
@@ -7,11 +5,10 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-
 from app import app
+
 covid_df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
-covid_df = covid_df[["iso_code",
-                    "continent",
+covid_df = covid_df[["continent",
                      "location",
                      "date",
                      "total_cases",
@@ -20,40 +17,109 @@ covid_df = covid_df[["iso_code",
                     "total_deaths",
                     "new_deaths",
                     "new_deaths_smoothed",]]
+
 covid_df = covid_df[covid_df["total_cases"].notna()]
-covid_df1 = covid_df[covid_df["iso_code"] = "OWID_WRL"]
+
+
+
+
+
 
 layout = html.Div(
     children=[
         html.Div(className='row',
                  children=[
-                    html.Div(className='two columns div-user-controls',
+                    html.Div(className='four columns div-user-controls',
                              children=[
-                                 html.H2('Coronavirus Dashboard'),
-                                 html.P('Homepage'),
-                                 html.P('A coronavirus dashboard made using Dash - Plotly'),
+                                 html.H2('Coronavirus National Statistics'),
+                                 html.P('Visualising Coronavirus statistcs for the selected country with Plotly - Dash.'),
+                                  html.Div(id='app-1-display-value')
+                                 
                                 ]
                              ),
                     html.Div(className='eight columns div-for-charts bg-grey',
                              children=[
-                                 dcc.Graph(id='graph5')
-                                    
-                             ])
+                                 dcc.Checklist(id='stat-select',
+                                                       options=[{'label': 'Total Cases', 'value': 'total_cases'},
+                                                                {'label': 'Total Deaths', 'value': 'total_deaths'},
+                                                                {'label': 'Daily Cases', 'value': 'new_cases'},
+                                                                {'label': 'Daily Deaths', 'value': 'new_deaths'}],
+                                                        value=["total_cases","total_deaths"],
+                                                       labelStyle={'display': 'inline-block'},
+                                                       style={"color": "white"}
+                                                      ),
+                                 dcc.Graph('country statistics2', config={'displayModeBar': False})
+                             ],style={'color': '#1E1E1E'}),
+                     
                               ])
         ]
 
 )
+       
+@app.callback(
+    Output('country statistics2', 'figure'),
+    [Input("stat-select","value")]
+    
+)
+        
 
-# @app.callback(
-#     Output('graph-with-slider', 'figure'),
-#     [Input('year-slider', 'value')])
+        
+def update_graph(stat):  
+   
+  
+#     stat= ["total_cases","total_deaths"]
 
-def update_global2():
+    
+    
+    
+    # Create figure
+    fig = go.Figure()
 
+    
+    for i in stat:
+            fig.add_trace(
+            go.Scatter(x=covid_df["date"][covid_df["location"]=="World"], y=covid_df[i][covid_df["location"]=="World"],
+            name=i))
 
-   fig = px.line(covid_df1, x="date", y="total_cases", title='Total Global Cases of Covid-19')
+        # Set title
+    fig.update_layout(
+        title_text="Time series with range slider and selectors"
+    )
 
-    fig.update_layout(height = 650,
-                      width = 900,
-                      template = "plotly_dark")
+    # Add range slider
+    fig.update_layout(height=700,
+                      width=900,
+                      template = 'plotly_dark',
+                      plot_bgcolor='rgb(30,30,30)',
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=1,
+                         label="YTD",
+                         step="year",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1y",
+                         step="year",
+                         stepmode="backward"),
+                    dict(step="all")
+                ]),
+              bgcolor = 'rgb(30,30,30)'
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+    )
+
+#     print(stat)
     return fig
